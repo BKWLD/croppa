75b664879ed0b75cd02e370d0a63b773d5ca519e
Robert Reinhard <robert.reinhard@bkwld.com>
2012-10-08 13:50:30 -0700

    Increase the memory limit of the server to allow big images to be resized

--- start.php
+++ start.php
@@ -12,6 +12,10 @@ Croppa::config(Config::get('croppa::croppa'));
 // Subscribe to 404 events, these are how Croppa gets triggered
 Event::listen('404', function() {
 	
+	// Increase memory limit, cause some images require a lot
+	// too resize
+	ini_set('memory_limit', '128M');
+	
 	// Pass Croppa the current URL
 	Croppa::handle_404(Request::uri());
 });
