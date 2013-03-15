dceaaca969b2973e6500cce7ebe1c0df8443398a
Robert Reinhard <robert.reinhard@bkwld.com>
2013-03-15 14:33:30 -0700

    Upping the default max crops

--- config/croppa.php
+++ config/croppa.php
@@ -9,6 +9,6 @@
 	// source file.  This is to limit scripts from filling
 	// up your hard drive with images.  Set to false or comment
 	// out to have no limit.
-	'max_crops' => 6,
+	'max_crops' => 12,
 	
 );
