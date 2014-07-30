db3b3b4ea7c533ad46b4e86edf0b243f0d60f294
Robert Reinhard <robert.reinhard@bkwld.com>
2014-07-30 10:59:12 -0700

    Updating unit tests to use a mocked fileystem
    via vfsStream

--- tests/TestDelete.php
+++ tests/TestDelete.php
@@ -1,43 +1,49 @@
 <?php
 
 use Bkwld\Croppa\Croppa;
+use org\bovigo\vfs\vfsStream;
 
 class TestDelete extends PHPUnit_Framework_TestCase {
 
-	public function dir() {
-		return __DIR__.'/delete_files';
-	}
+	private $root;
+	private $croppa;
+	public function setUp() {
 
-	public function make() {
-		return new Croppa(array(
-			'src_dirs' => array($this->dir()),
+		// Make an image
+		$gd = imagecreate(200, 100);
+		ob_start();
+		imagejpeg($gd);
+		$image = ob_get_clean();
+
+		// Make virtual fileystem
+		$this->root = vfsStream::setup('dir', null, array(
+			'me.jpg' => $image,
+			'me-200x100.jpg' => $image,
+			'me-and-you.jpg' => $image,
+		));
+
+		// Share a croppa instance
+		$this->croppa = new Croppa(array(
+			'src_dirs' => array(vfsStream::url('dir')),
 			'max_crops' => 12,
 		));
 	}
 
 	public function testContainsSelf() {
-		$relative = 'me.jpg';
-		$absolute = $this->dir().'/'.$relative;
-		$croppa = $this->make();
-		$this->assertTrue(in_array($absolute, $croppa->findFilesToDelete($relative)));
+		$this->assertContains(vfsStream::url('dir/me.jpg'), $this->croppa->findFilesToDelete('me.jpg'));
 	}
 
 	// https://github.com/BKWLD/croppa/issues/32
 	public function testContainsDoesNotContainSimilar() {
-		$relative = 'me.jpg';
-		$similar = $this->dir().'/me-and-you.jpg';
-		$croppa = $this->make();
-		$this->assertFalse(in_array($similar, $croppa->findFilesToDelete($relative)));
+		$this->assertNotContains(vfsStream::url('dir/me-and-you.jpg'), $this->croppa->findFilesToDelete('me.jpg'));
 	}
 
 	public function testLength() {
-		$croppa = $this->make();
-		$this->assertEquals(2, count($croppa->findFilesToDelete('me.jpg')));
+		$this->assertEquals(2, count($this->croppa->findFilesToDelete('me.jpg')));
 	}
 
 	public function testBadURL() {
-		$croppa = $this->make();
-		$this->assertEquals(0, count($croppa->findFilesToDelete('whatever.dude')));
+		$this->assertEquals(0, count($this->croppa->findFilesToDelete('whatever.dude')));
 	}
 
 }
