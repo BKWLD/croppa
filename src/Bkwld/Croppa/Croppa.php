e40092e0942dcd70874bd31c7e13f70d2950b079
Robert Reinhard <robert.reinhard@bkwld.com>
2015-02-09 13:00:14 -0800

    Generate should actually deal with the absolute url, not the path

--- src/Bkwld/Croppa/Croppa.php
+++ src/Bkwld/Croppa/Croppa.php
@@ -94,7 +94,7 @@ class Croppa {
 		
 		// See if the referenced file exists and is an image.  This gives us the absolute
 		// to the image, given the $path which is relative to a src_dir
-		if (!($src = $this->checkForFileByPath($path))) throw new Exception('Croppa: Referenced file missing');
+		if (!($src = $this->checkForFile($path))) throw new Exception('Croppa: Referenced file missing');
 		
 		// Put the croped output in the same directory as the src image
 		$dst = dirname($src).'/'.basename($url);
