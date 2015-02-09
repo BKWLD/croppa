cb5e8f8ba8457d3b2d87bd8e38b7d3f32ac7b1a5
Robert Reinhard <robert.reinhard@bkwld.com>
2015-02-09 12:24:58 -0800

    Testing deletes within subdirs

--- tests/TestDelete.php
+++ tests/TestDelete.php
@@ -2,6 +2,7 @@
 
 use Bkwld\Croppa\Croppa;
 use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\visitor\vfsStreamStructureVisitor;
 
 class TestDelete extends PHPUnit_Framework_TestCase {
 
@@ -17,43 +18,55 @@ class TestDelete extends PHPUnit_Framework_TestCase {
 
 		// Make virtual fileystem
 		$this->root = vfsStream::setup('dir', null, array(
-			'me.jpg' => $image,
-			'me-200x100.jpg' => $image,
-			'me-and-you.jpg' => $image,
+			'uploads' => array(
+				'00' => array(
+					'me.jpg' => $image,
+					'me-200x100.jpg' => $image,
+					'me-and-you.jpg' => $image,
+				)
+			)
 		));
 
 		// Share a croppa instance
 		$this->croppa = new Croppa(array(
 			'public' => vfsStream::url('dir'),
-			'src_dirs' => array(vfsStream::url('dir')),
-			'max_crops' => 12,
+			'src_dirs' => array(vfsStream::url('dir/uploads')),
 		));
 	}
 
 	public function testContainsSelf() {
-		$this->assertContains(vfsStream::url('dir/me.jpg'), $this->croppa->findFilesToDelete('me.jpg'));
+		$this->assertContains(
+			vfsStream::url('dir/uploads/00/me.jpg'), 
+			$this->croppa->findFilesToDelete('/uploads/00/me.jpg')
+		);
 	}
 
 	// https://github.com/BKWLD/croppa/issues/32
 	public function testContainsDoesNotContainSimilar() {
-		$this->assertNotContains(vfsStream::url('dir/me-and-you.jpg'), $this->croppa->findFilesToDelete('me.jpg'));
+		$this->assertNotContains(
+			vfsStream::url('dir/uploads/00/me-and-you.jpg'), 
+			$this->croppa->findFilesToDelete('/uploads/00/me.jpg')
+		);
 	}
 
 	public function testFindCrops() {
-		$this->assertEquals(2, count($this->croppa->findFilesToDelete('me.jpg')));
+		$this->assertEquals(2, count($this->croppa->findFilesToDelete('/uploads/00/me.jpg')));
 	}
 
 	public function testNotOriginal() {
-		$this->assertEquals(1, count($this->croppa->findFilesToDelete('me.jpg', false)));
+		$this->assertEquals(1, count($this->croppa->findFilesToDelete('/uploads/00/me.jpg', false)));
 	}
 
 	public function testBadURL() {
-		$this->assertEquals(0, count($this->croppa->findFilesToDelete('whatever.dude')));
+		$this->assertEquals(0, count($this->croppa->findFilesToDelete('/uploads/00/whatever.jpg')));
 	}
 
 	// https://github.com/BKWLD/croppa/issues/48
 	public function testFilenameWithDimensions() {
-		$this->assertEquals(array(vfsStream::url('dir/me-200x100.jpg')), $this->croppa->findFilesToDelete('me-200x100.jpg'));
+		$this->assertEquals(
+			array(vfsStream::url('dir/uploads/00/me-200x100.jpg')), 
+			$this->croppa->findFilesToDelete('/uploads/00/me-200x100.jpg')
+		);
 	}
 
 }
