d4b83a6dcb3e29c66886fa3fa792765dcdb2caa5
Robert Reinhard <robert.reinhard@bkwld.com>
2015-04-07 16:12:27 -0700

    Fixing calls to delete and reset
    And ditching the call_user_func approach

--- src/Bkwld/Croppa/Helpers.php
+++ src/Bkwld/Croppa/Helpers.php
@@ -29,38 +29,52 @@ class Helpers {
 
 	/**
 	 * Delete source image and all of it's crops
-	 * 
+	 *
+	 * @param string $url 
+	 * @return void 
 	 * @see Bkwld\Croppa\Storage::deleteSrc()
 	 */
-	public function delete() {
-		return call_user_func_array([$this->storage, 'deleteSrc'], func_get_args());
+	public function delete($url) {
+		return $this->storage->deleteSrc($this->url->relativePath($url));
 	}
 
 	/**
 	 * Delete just the crops, leave the source image
-	 * 
+	 *
+	 * @param string $url 
+	 * @return void 
 	 * @see Bkwld\Croppa\Storage::deleteCrops()
 	 */
-	public function reset() {
-		return call_user_func_array([$this->storage, 'deleteCrops'], func_get_args());
+	public function reset($url) {
+		return $this->storage->deleteCrops($this->url->relativePath($url));
 	}
 
 	/**
 	 * Create an image tag rather than just the URL.  Accepts the same params as url()
 	 *
+	 * @param string $url URL of an image that should be cropped
+	 * @param integer $width Target width
+	 * @param integer $height Target height
+	 * @param array $options Addtional Croppa options, passed as key/value pairs.  Like array('resize')
+	 * @return string An HTML img tag for the new image
 	 * @see Bkwld\Croppa\URL::generate()
 	 */
-	public function tag() {
-		return '<img src="'.call_user_func_array([$this->url, 'url'], func_get_args()).'">';
+	public function tag($url, $width = null, $height = null, $options = null) {
+		return '<img src="'.$this->url->generate($url, $width, $height, $options).'">';
 	}
 
 	/**
 	 * Pass through URL requrests to URL->generate().
 	 *
+	 * @param string $url URL of an image that should be cropped
+	 * @param integer $width Target width
+	 * @param integer $height Target height
+	 * @param array $options Addtional Croppa options, passed as key/value pairs.  Like array('resize')
+	 * @return string The new path to your thumbnail
 	 * @see Bkwld\Croppa\URL::generate()
 	 */
-	public function url() {
-		return call_user_func_array([$this->url, 'generate'], func_get_args());
+	public function url($url, $width = null, $height = null, $options = null) {
+		return $this->url->generate($url, $width, $height, $options);
 	}
 
 }
